514f511b9854248571e022b8cc844cdaf0d7330e
accelsao <jay70550@yahoo.com.tw>
2019-04-06 12:22:03 +0800

    update pixiv

---
+++ pixiv/pixiv/README.md
@@ -0,0 +1,21 @@
+- [X] search keyword sort_key= newest
+- [X] search keyword sort_key= bookmark
+- [ ] search keyword sort_key= bookmark accelerate
+- [X] get ranking mode weekly
+- [ ] get ranking mode daily
+- [ ] get ranking mode monthly
+- [ ] get specific author's works
+-[ ] add multithread
+-[ ] add time count
+
+
+
+[Online regex tester and debugger](https://regex101.com/r/tUxrBG/1)
+
+## Reference
+[从零开始的Pixiv排行榜爬虫](https://zhuanlan.zhihu.com/p/26685820)
+
+[python爬蟲學習–pixiv爬蟲(1)–p站爬蟲的登入](https://codertw.com/%E7%A8%8B%E5%BC%8F%E8%AA%9E%E8%A8%80/538948/)
+
+
+[多线程P站爬虫](https://github.com/f1shh/web-crewler-for-pixiv/blob/master/pixiv.py)
--- pixiv/pixiv/main.py
+++ pixiv/pixiv/main.py
@@ -3,6 +3,8 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import os
+import heapq
+
 
 class Pixiv():
     def __init__(self):
@@ -15,8 +17,8 @@ class Pixiv():
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
         }
         self.post_key = self.get_post_key()
-        self.pixiv_id = ''
-        self.password = ''
+        self.pixiv_id = 'sayuri002'
+        self.password = 'jay1000359'
         self.return_to = 'https://www.pixiv.net/'
 
         self.ranking_url = 'https://www.pixiv.net/ranking.php'
@@ -76,10 +78,11 @@ class Pixiv():
         if not os.path.exists(path):
             os.makedirs(path)
 
-    def get_keyword_topN(self, search_keyword=None, page_num=10):
+    def get_keyword_topN(self, search_keyword=None, page_num=10, bookmarkThreshold=10000):
         keyword = str(search_keyword.encode('utf-8'))[2:-1].replace('\\x','%')
         # print(keyword)
         url = 'https://www.pixiv.net/search.php?word={}&order=date_d&p='
+        image_list = []
         for page in range(page_num):
             target_url = url.format(keyword) + str(page + 1)
             # print(target_url)
@@ -90,19 +93,35 @@ class Pixiv():
             data_list = soup.find_all('input', {'id': 'js-mount-point-search-result-list'})[0] # 大膽假設每頁只有一個result list
             data = data_list['data-items']
             # print(data)
-            id_list = re.findall('"illustId":"(.+?)"', data)[:3]
+            id_list = re.findall('"illustId":"(.+?)"', data)
 
             for id in id_list:
                 id_url = self.target_url.format(id)
-                print(id_url)
+                # print(id_url)
                 html = self.session.get(id_url, headers=self.headers).text
-                print(html)
+                # print(html)
+                img_bookmarkCount = re.search('"bookmarkCount":(\d+),', html).group(1)
+                if int(img_bookmarkCount) < bookmarkThreshold:
+                    continue
+                # print(img_bookmarkCount)
                 img_src = re.search('"regular":"(.+?)",', html).group(1)
                 img_src = img_src.replace('\\', '')
                 img_title = re.search('title>(.*?)】「(.*?)」', html).group(2)
-                print(img_src)
-                print(img_title)
+                # print(img_src)
+                # print(img_title)
+                # image_list.append({
+                #     'url': img_src,
+                #     'title': img_title,
+                #     'bookmarkCount': int(img_bookmarkCount),
+                #     'href': id_url
+                # })
                 self.download_image(img_src, img_title, id_url)
+            print('Page {} Finished'.format(page + 1))
+        # image_list = sorted(image_list, key=lambda k: k['bookmarkCount'], reverse=True)
+        # for a in image_list:
+        #     self.download_image(a['url'], a['title'], a['href'])
+            # print(a['bookmarkCount'])
+            #     self.download_image(img_src, img_title, id_url)
 
 
 
@@ -112,5 +131,5 @@ class Pixiv():
 
 pixiv = Pixiv()
 # pixiv.get_ranking(img_num=10)
-pixiv.get_keyword_topN('女の子', page_num=1)
+pixiv.get_keyword_topN('少女前線', page_num=8, bookmarkThreshold=1000)  # 超慢
 
